3f55a3bae3515a9e73dcd982608c05e306e19a7c
csaffold <ceciliasaffold@gmail.com>
2020-10-12 21:00:24 -0500

    Completed ArrayExercises

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,26 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] integers = { 1, 1, 2, 3, 5, 8 };
+
+            //list each integer in the array integers in order
+            for (int i = 0; i < integers.Length; i++)
+            {
+                Console.WriteLine(integers[i]);
+            }
+            
+            //another way to list each integer in array in order on separate lines
+            foreach (int i in integers)
+            {
+                Console.WriteLine(i);
+            }
+            
+            //list only the odd integers in array
+            for (int i = 0; i < integers.Length; i++)
+                if (integers[i] % 2 !=0)
+                {
+                    Console.WriteLine(integers[i]);
+                }
         }
     }
 }
