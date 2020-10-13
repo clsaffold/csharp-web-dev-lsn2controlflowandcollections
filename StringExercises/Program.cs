e0d3ea5f647d94de8a6bbc90cfe55052921b5e39
csaffold <ceciliasaffold@gmail.com>
2020-10-12 21:29:16 -0500

    Completed String Exercises

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,20 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string exercisePhrase = "I would not, could not, in a box. I would not, could not with a fox.\nI will not eat them in a house. I will not eat them with a mouse.";
+
+            string[] phraseWords = exercisePhrase.Split(' ');
+            string[] sentences = exercisePhrase.Split('.');
+            
+            foreach (var word in phraseWords)
+            {
+                Console.WriteLine($"{word}");
+            }
+
+            foreach (var sentence in sentences)
+            {
+                Console.WriteLine($"{sentence}");
+            }
         }
     }
 }
